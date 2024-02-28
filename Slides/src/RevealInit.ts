ca2021997b4cb67c05bed73436921ceaeffe7a8c
Andrew King <pepsiotaku@gmail.com>
2024-02-28 10:06:11 -0500

    use ts

---
+++ Slides/src/RevealInit.ts
@@ -0,0 +1,22 @@
+import Reveal from "reveal.js";
+import RevealNotes from "reveal.js/plugin/notes/notes";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight";
+
+import { regesterYamlPatch } from "./HighlightYaml";
+
+const options: Reveal.Options = {
+  hash: true,
+  controls: false,
+  controlsBackArrows: "faded",
+  plugins: [RevealHighlight, RevealNotes],
+  autoAnimateEasing: "ease-out",
+  autoAnimateDuration: 0.25,
+  autoAnimateUnmatched: false,
+  highlight: {
+    beforeHighlight: (hljs) => {
+      regesterYamlPatch(hljs);
+    },
+  },
+};
+
+Reveal.initialize(options);
