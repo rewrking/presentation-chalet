c0a545a404af0e021be564e94005bfc95a79ef50
Andrew King <pepsiotaku@gmail.com>
2024-03-01 08:32:18 -0500

    set base

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+	base: "/presentation-chalet/",
 	build: { chunkSizeWarningLimit: 1200, minify: true }
 });
